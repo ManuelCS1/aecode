87372eabdd4ad73987baf025eac017fe75d34209
ManuelCS1 <manuelcs201002@gmail.com>
2024-05-02 10:11:58 -0500

    Configuracion Crud

--- src/main/java/ai/aecode/aecode/AecodeApplication.java
+++ src/main/java/ai/aecode/aecode/AecodeApplication.java
@@ -2,6 +2,9 @@ package ai.aecode.aecode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AecodeApplication {
@@ -10,4 +13,13 @@ public class AecodeApplication {
         SpringApplication.run(AecodeApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://aecode.onrender.com/").allowedMethods("*").allowedHeaders("*");
+            }
+        };
+    }
 }
